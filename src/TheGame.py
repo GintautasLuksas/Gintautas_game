871f896d265bf431b46fe6ecb0f92d652702fd74
GintautasLuksas <167891054+GintautasLuksas@users.noreply.github.com>
2024-05-25 18:40:19 +0300

    Update TheGame.py
    Added class Monster, updated with one fight. Not in game yet.

--- src/TheGame.py
+++ src/TheGame.py
@@ -1,4 +1,3 @@
-
 class Stats:
     def __init__(self, HP, MP, STR, AGI, GEAR, INVENTORY, GOLD):
         self.hp = HP
@@ -16,9 +15,71 @@ class Stats:
                 f"INVENTORY: {self.inventory}\n"
                 f"GOLD: {self.gold}")
 
-# Initialize global player stats
+class Monster:
+    def __init__(self, name: str, life: int, damage: int):
+        self.name = name
+        self.life = life
+        self.damage = damage
+
+    def attack(self, hit_to_monster):
+        self.life -= hit_to_monster
+
+    def loot(self, inventory, item):
+        inventory.append(item)
+
+import random
+
 player_stats = Stats(100, 100, 10, 5, ['Leather gloves', 'Leather boots', 'Leather armor', 'Bronze sword'], [], 0)
 
+#Monster Whelp Fight
+whelp = Monster('Whelp', 20, 10)
+while True:
+    attack = random.randint(1, 4)
+    user_input = int(input('''
+The fight is on. Choose your action:
+1. Stay at place, so you can attack.
+2. Move left to avoid fire.
+3. Move right to avoid it jumping on you.
+4. Hide behind rock to avoid flight attack..'''))
+
+    if user_input == 1 and attack == 1:
+        print(
+            'You hide behind rock, whelp tried to get in the air and hit you with his face, but hit rock. It is dead!')
+        whelp.loot(player_stats.inventory, 'Bronze Helm')
+        print(f'''
+        You defeated the monster and got a Bronze Helm!
+        Inventory update: {player_stats.inventory}''')
+        break
+    elif user_input == 2 and attack == 2:
+        print(
+            'You hide behind rock, whelp tried to get in the air and hit you with his face, but hit rock. It is dead!')
+        whelp.loot(player_stats.inventory, 'Bronze Helm')
+        print(f'''
+        You defeated the monster and got a Bronze Helm!
+        Inventory update: {player_stats.inventory}''')
+        break
+    elif user_input == 3 and attack == 3:
+        print(
+            'You hide behind rock, whelp tried to get in the air and hit you with his face, but hit rock. It is dead!')
+        whelp.loot(player_stats.inventory, 'Bronze Helm')
+        print(f'''
+        You defeated the monster and got a Bronze Helm!
+        Inventory update: {player_stats.inventory}''')
+        break
+    elif user_input == 4 and attack == 4:
+        print('You hide behind rock, whelp tried to get in the air and hit you with his face, but hit rock. It is dead!')
+        whelp.loot(player_stats.inventory, 'Bronze Helm')
+        print(f'''
+You defeated the monster and got a Bronze Helm!
+Inventory update: {player_stats.inventory}''')
+        break
+    else:
+        print(f'Oh no! You guessed {user_input} scenario. But the dragon had {attack} scenario!')
+        player_stats.hp -= whelp.damage
+        print(f'You have {player_stats.hp} left!')
+        if player_stats.hp <= 0:
+            print("You died!")
+            continue
 def main_menu():
     while True:
         user_choice = input('''Welcome! Select option:
