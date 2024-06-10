e7d6e38737265becc90dcfb69087a88a4615c7e8
Gintautas Lukšas <gintautas.luksas@gmail.com>
2024-06-10 21:55:15 +0300

    upx

--- src/Stats.py
+++ src/Stats.py
@@ -1,16 +1,14 @@
 class Stats:
-    def __init__(self, HP, MP, STR, AGI, GEAR, INVENTORY, GOLD):
+    def __init__(self, HP, MP, STR, AGI, GEAR, GOLD):
         self.hp = HP
         self.mp = MP
         self.str = STR
         self.agi = AGI
         self.gear = GEAR
-        self.inventory = INVENTORY
         self.gold = GOLD
 
     def __str__(self):
         return (f"HP: {self.hp}, MP: {self.mp}\n"
                 f"STR: {self.str}, AGI: {self.agi}\n"
                 f"GEAR: {self.gear}\n"
-                f"INVENTORY: {self.inventory}\n"
                 f"GOLD: {self.gold}")
